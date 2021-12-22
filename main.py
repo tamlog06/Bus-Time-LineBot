87106221aea152ef55ef1af00bc8c8cf6148fc9b
tam1006 <tamusyun1006@gmail.com>
2021-12-22 14:04:40 +0900

    fix bug

--- main.py
+++ main.py
@@ -124,9 +124,11 @@ def handle_message(event):
         # imgs = soup.find_all('img', src="./disp_image_sp/bus_now_app_img_sp.gif")
         text = ''
         for i in range(len(imgs)):
+            tx = imgs[i].get('src')
             line_bot_api.push_message(
                     event.source.user_id,
-                    TextSendMessage(text=imgs[i].get('src')))
+                    TextSendMessage(text=f'{tx}'))
+        
         for i in range(len(imgs)):
             if imgs[i].get('src') == './disp_image_sp/bus_now_app_img_sp.gif':
                 text = f'{i+1}駅前を過ぎました。もうすぐ到着します。'
