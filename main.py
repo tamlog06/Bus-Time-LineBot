4f43c9261cec056119ae4229b4b83fda6f1491ac
tam1006 <tamusyun1006@gmail.com>
2022-06-06 00:57:17 +0900

    clean up code

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from bs4 import BeautifulSoup
 import time
 import re
 import pickle
+
 app = Flask(__name__)
 
 #環境変数取得
@@ -27,12 +28,13 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 class Text:
     def __init__(self):
-        self.error = {'url': 'URLが正しくありません。\nポケロケのサイトから目的のバス停のバス接近情報を表示するURLを入力してください。\n詳しい使い方は〜を参照してください。\nhttp://blsetup.city.kyoto.jp/blsp/',\
-            'imgs': '表示するバスが複数選択されてしまっています。\n表示するバスは1つだけ選択してください。', 'no_url': 'URLが設定されていません。\nポケロケのサイトから目的のバス停のバス接近情報を表示するURLを入力してください。\n詳しい使い方は〜を参照してください。\nhttp://blsetup.city.kyoto.jp/blsp/'}
-        self.start = '一番近いバスの接近状況を通知します。\n10分経過で自動終了します。'
-        self.end = {'quit_flag': '終了します。', 'time': '10分が経過したので終了します。', 'arrive': 'バスが到着したので終了します。'}
-        self.bus = {1: '1駅前を以下のバスが通過しました。\nもうすぐ到着します。', 2: '2駅前を以下のバスが通過しました。', 3: '3駅前を以下のバスが通過しました。', 4: '近くにまだバスがいません。', 'arrive': 'バスが到着しました。'}
-        self.follow = '友達追加ありがとうございます！\nポケロケのサイトから目的のバス停のバス接近情報を表示するURLを入力してもらうと、バス接近情報を通知します。\n詳しい使い方は〜を参照してください。\nhttp://blsetup.city.kyoto.jp/blsp/'
+        self.error = {'url': 'URL間違ってるで。\nポケロケのサイトから目的のバス停のバス接近情報を表示するURLを入力してや。\n乗りたいバス停の名前を入力してな。\nその後表示されるURLから、乗りたいバスを選んで、「決定」→「設定バス接近情報を見る」と進んだ後の画面のURLを入力してくれ。',\
+            'no_url': 'URLが設定されてないで。\nポケロケのサイトから目的のバス停のバス接近情報を表示するURLを入力してや。\n乗りたいバス停の名前を入力してな。\nその後表示されるURLから、乗りたいバスを選んで、「決定」→「設定バス接近情報を見る」と進んだ後の画面のURLを入力してくれ。',\
+            'starting': '既に起動中やで。他の設定にしたかったら、「終了」と入力してからまた新しく登録し直してくれや。'}
+        self.start = '一番近いバスの接近状況を通知するで。\n10分経過で自動終了するしな。'
+        self.end = {'quit_flag': '終了するで。', 'time': '10分が経過したので終了するで。', 'arrive': 'バスが到着したから終了するな。'}
+        self.bus = {1: '1駅前を以下のバスが通過したで。\nもうすぐ到着するで。\n急いでや。', 2: '2駅前を以下のバスが通過したで。', 3: '3駅前を以下のバスが通過したで。', 4: '近くにまだバスおらんで。', 'arrive': 'バスが到着したわ。\nちゃんと乗れたか？'}
+        self.follow = '友達追加ありがとう！\n乗りたいバス停の名前を入力してな。\nその後表示されるURLから、乗りたいバスを選んで、「決定」→「設定バス接近情報を見る」と進んだ後の画面のURLを入力してくれ。\n一番近いバスが何駅前におるか教えたるで。'
     
     def return_bus_text(self, bus_id, keitoList):
         assert type(bus_id) == int
@@ -62,12 +64,12 @@ class User:
     def set_run_flag(self, event, flag):
         self.run_flags[event.source.user_id] = flag
 
-users = User()
-txt = Text()
+userClass = User()
+textClass = Text()
 
 # 各バス停のバス停番号を辞書方で保存したもの
 with open('station.pkl', 'rb') as f:
-    station = pickle.load(f)
+    stationDict = pickle.load(f)
 
 #Webhookからのリクエストをチェック
 @app.route("/callback", methods=['POST'])
@@ -97,105 +99,117 @@ def callback():
 def handle_message(event):
     # ユーザーが終了メッセージを送った場合はフラグを立てて通知を終了する
     if event.message.text == "終了":
-        users.set_quit_flag(event, True)
+        userClass.set_quit_flag(event, True)
         return
 
-    # 起動中であれば何もしない
+    # 起動中かどうかのフラグ
     try:
-        run_flag = users.run_flags[event.source.user_id]
+        run_flag = userClass.run_flags[event.source.user_id]
     except KeyError:
-        users.add_user(event)
-        run_flag = users.run_flags[event.source.user_id]
+        userClass.add_user(event)
+        run_flag = userClass.run_flags[event.source.user_id]
     
+    # 起動中であればエラーを返す
     if run_flag:
+        line_bot_api.reply_message(
+            event.reply_token, 
+            TextSendMessage(text=textClass.error['starting'])
+        )
         return
     
     # 既にURLが設定されている場合は、そのURLを使用する
     if event.message.text == '開始':
         # 開始メッセージが送られてきたのにURLが設定されていない場合
-        if not event.source.user_id in users.url:
+        if not event.source.user_id in userClass.url:
             line_bot_api.reply_message(
                 event.reply_token, 
-                TextSendMessage(text=txt.error['no_url']))
+                TextSendMessage(text=textClass.error['no_url'])
+            )
             return
     
-    # ユーザーがバス停の名前を送ってきた場合は、対応するURLを送信する
+    # ユーザーがバス停のリンク以外を送った場合
     elif not event.message.text.startswith('http'):
         text = event.message.text
-        if text in station.keys():
-            station_id = station[text]
+        # 正しいバス停の名前が送られてきた場合
+        if text in stationDict.keys():
+            station_id = stationDict[text]
             url = f'http://blsetup.city.kyoto.jp/blsp/step3.php?id={station_id}'
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=url)
             )
+            return
+        
+        # バス停名が見つからない場合は、近そうなものを探す（上限5個）
+        candidates = candidate_names(text)
+        # 一致するものがない場合
+        if len(candidates) == 0:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='そんなバス停ないで')
+            )
+            return
+        elif len(candidates) == 1:
+            station_id = stationDict[candidates[0]]
+            url = f'http://blsetup.city.kyoto.jp/blsp/step3.php?id={station_id}'
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=url)
+            )
+            return
         else:
-            # バス停名が見つからない場合は、近そうなものを探す（上限5個）
-            candidates = candidate_names(text)
-            if len(candidates) == 0:
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='そんなバス停ないよ')
-                )
-            elif len(candidates) == 1:
-                station_id = station[candidates[0]]
-                url = f'http://blsetup.city.kyoto.jp/blsp/step3.php?id={station_id}'
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text=url)
-                )
-            else:
-                candidates = '\n'.join(candidates)
-                text = f'どれ？\n{candidates}'
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text=text)
-                )
-        return
-
+            candidates = '\n'.join(candidates)
+            text = f'どれ？\n{candidates}'
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=text)
+            )
+            return
 
     # 正しいURLかどうかチェック
     elif not check_error(event):
         return
     
-    users.set_run_flag(event, True)
-    users.set_quit_flag(event, False)
-    response = requests.get(users.url[event.source.user_id])
+    # 開始処理
+    userClass.set_run_flag(event, True)
+    userClass.set_quit_flag(event, False)
+    response = requests.get(userClass.url[event.source.user_id])
     soup = BeautifulSoup(response.text, 'html.parser')
     title = soup.find('title')
     title = re.findall('：.*：', title.text)[0][1:-1]
-    text = f'{title}\n{txt.start}'
+    text = f'{title}\n{textClass.start}'
     line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=text))
+        event.reply_token,
+        TextSendMessage(text=text)
+    )
     
     start_time = time.time()
     now = start_time
-    before_text = ''
-    before_text_id = -1
-    bus_num_before = -1
+    before_message = ''
+    before_message_id = -1
+    before_nbus = -1
     before_keitoList = []
-    # 時間制限で終了したかどうかのフラグ
-    finish_flag = False
+    # バスが来たかどうかのフラグ
+    arrive_flag = False
 
     # 10分間
     while now - start_time < 600:
         # ユーザークラスから、各ユーザーのフラグを取得
         try:
-            quit_flag = users.quit_flags[event.source.user_id]
+            quit_flag = userClass.quit_flags[event.source.user_id]
         except KeyError:
-            users.add_user(event)
-            quit_flag = users.quit_flags[event.source.user_id]
+            userClass.add_user(event)
+            quit_flag = userClass.quit_flags[event.source.user_id]
         
         if quit_flag:
             break
 
-        # busimgクラスのimgを取得
-        response = requests.get(users.url[event.source.user_id])
+        # bls-keito-num-imgクラス（バスの系統の取得）とbusimgクラス（バスが存在するかどうかの判定）のimgを取得
+        response = requests.get(userClass.url[event.source.user_id])
         soup = BeautifulSoup(response.text, 'html.parser')
         imgs = soup.find_all('img', class_=['bls-keito-num-img', 'busimg'])
 
-        bus_num = 0
+        nbus = 0
         text_id = 4
         keitoList = []
         
@@ -214,72 +228,59 @@ def handle_message(event):
                     elif text_id > i%4:
                         text_id = i%4
                         keitoList = [keito]
-                    bus_num += 1
-
-        # for i, img in enumerate(imgs):
-        #     if img.get('src') == "./disp_image_sp/bus_now_app_img_sp.gif" or img.get('src') == './disp_image_sp/bus_img_sp.gif':
-        #         text_id = min(text_id, i%3)
-        #         bus_num += 1
+                    nbus += 1
         
-        text = txt.return_bus_text(text_id, keitoList)
-
-
-        # for i in range(len(imgs)):
-        #     # now_appが1駅前、bus_imgが2、3駅前のときのもの
-        #     if imgs[i].get('src') == './disp_image_sp/bus_now_app_img_sp.gif':
-        #         bus_num += 1
-        #         if text == '':
-        #             text = txt.bus['1']
-        #     elif  imgs[i].get('src') ==  './disp_image_sp/bus_img_sp.gif':
-        #         bus_num += 1
-        #         if text == '':
-        #             text = txt.bus[str(i+1)]
+        # 送るメッセージ
+        message = textClass.return_bus_text(text_id, keitoList)
         
-        # 前の通知が1駅前のもので、現在のバスの数が前のものより少なければ、バスが到着したと判断して終了
-        if before_text_id == 1 and (bus_num < bus_num_before or text_id != 1 or len(keitoList) < len(before_keitoList)):
+        # 前の通知が1駅前のもので、現在のバスの数が前のものより少ないか、一番近いバスが２駅前になったか、一番近いバスの系統の数が少なくなったら、バスが到着したと判定して終了
+        if before_message_id == 1 and (nbus < before_nbus or text_id != 1 or len(keitoList) < len(before_keitoList)):
             line_bot_api.push_message(
                 event.source.user_id,
-                TextSendMessage(text=txt.bus['arrive']))
-            finish_flag = True
+                TextSendMessage(text=textClass.bus['arrive']))
+            arrive_flag = True
             break
 
         # 前のサイクルで通知した内容と状況が変われば通知する
-        if text != before_text:
+        if message != before_message:
             line_bot_api.push_message(
                 event.source.user_id,
-                TextSendMessage(text=text))
+                TextSendMessage(text=message))
         
-        before_text = text
-        before_text_id = text_id
-        bus_num_before = bus_num
+        before_message = message
+        before_message_id = text_id
+        before_nbus = nbus
         before_keitoList = keitoList
+
+        # アクセス負荷を下げるため10秒待つ
         time.sleep(10)
-        # t += 10
+
         now = time.time()
     
     # ユーザーがフラグを立てて終了した時
-    if users.quit_flags[event.source.user_id]:
-        text = txt.end['quit_flag']
+    if userClass.quit_flags[event.source.user_id]:
+        message = textClass.end['quit_flag']
     # バスが到着して終了した時
-    elif finish_flag:
-        text = txt.end['arrive']
+    elif arrive_flag:
+        message = textClass.end['arrive']
     # 時間制限で終了した時
     else:
-        text = txt.end['time']
+        message = textClass.end['time']
     
-    users.set_quit_flag(event, False)
-    users.set_run_flag(event, False)
+    userClass.set_quit_flag(event, False)
+    userClass.set_run_flag(event, False)
     line_bot_api.push_message(
         event.source.user_id,
-        TextSendMessage(text=text)
+        TextSendMessage(text=message)
     )
+    return
 
 @handler.add(FollowEvent)
 def handle_follow(event):
-    users.add_user(event)
+    userClass.add_user(event)
     app.logger.info("Got Follow event:" + event.source.user_id)
     line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=txt.follow))
+        event.reply_token, TextSendMessage(text=textClass.follow))
 
 def check_error(event):
     try:
@@ -288,33 +289,33 @@ def check_error(event):
         # ステータスコードが200以外ならエラー
         response.raise_for_status()
 
+        # busimgクラスのimgを取ってくる
         soup = BeautifulSoup(response.text, 'html.parser')
         imgs = soup.find_all('img', class_='busimg')
         title = soup.find('title')
-        # 複数でも可能にする
-        # if len(imgs) > 3:
-        #     line_bot_api.push_message(
-        #         event.source.user_id,
-        #         TextSendMessage(text=txt.error['imgs']))
-        #     return False
+
+        # 正しく設定されていれば、画像の数は3個以上になるはず
         if title == None or title.text == [] or len(imgs) < 3:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(ext=txt.error['url']))
+                TextSendMessage(ext=textClass.error['url']))
             return False
         
-        users.add_URL(event)
+        # 問題なければurlを割り当ててTrueを返す
+        userClass.add_URL(event)
         return True
 
     except requests.exceptions.MissingSchema or requests.exceptions.ConnectionError or requests.exceptions.HTTPError:
         line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=txt.error['url']))
+            event.reply_token,
+            TextSendMessage(text=textClass.error['url'])
+        )
         return False
 
+# 近い名前のバス停を5個まで探索する
 def candidate_names(name):
     candidate_names = []
-    for key in station.keys():
+    for key in stationDict.keys():
         if name in key:
             candidate_names.append(key)
         
@@ -322,7 +323,6 @@ def candidate_names(name):
             return candidate_names
     
     return candidate_names
-
 
 # ポート番号の設定
 if __name__ == "__main__":
