6f5df9bf83a3bc6f3ced24419892a898dee9259a
tam1006 <tamusyun1006@gmail.com>
2022-06-06 00:59:14 +0900

    change message

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def handle_message(event):
             return
         else:
             candidates = '\n'.join(candidates)
-            text = f'どれ？\n{candidates}'
+            text = f'この中にある？\n{candidates}'
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=text)
