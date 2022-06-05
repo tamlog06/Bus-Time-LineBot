85fd5c055af7c42e28d3e9a626e021fd61e74100
tam1006 <tamusyun1006@gmail.com>
2022-06-05 18:00:17 +0900

    add new feature: show keito number

--- main.py
+++ main.py
@@ -31,8 +31,17 @@ class Text:
             'imgs': '表示するバスが複数選択されてしまっています。\n表示するバスは1つだけ選択してください。', 'no_url': 'URLが設定されていません。\nポケロケのサイトから目的のバス停のバス接近情報を表示するURLを入力してください。\n詳しい使い方は〜を参照してください。\nhttp://blsetup.city.kyoto.jp/blsp/'}
         self.start = '一番近いバスの接近状況を通知します。\n15分経過で自動終了します。'
         self.end = {'quit_flag': '終了します。', 'time': '15分が経過したので終了します。', 'arrive': 'バスが到着したので終了します。'}
-        self.bus = {0: '1駅前をバスが通過しました。\nもうすぐ到着します。', 1: '2駅前をバスが通過しました。', 2: '3駅前をバスが通過しました。', 3: '近くにまだバスがいません。', 'arrive': 'バスが到着しました。'}
+        self.bus = {1: '1駅前を以下のバスが通過しました。\nもうすぐ到着します。', 2: '2駅前を以下のバスが通過しました。', 3: '3駅前を以下のバスが通過しました。', 4: '近くにまだバスがいません。', 'arrive': 'バスが到着しました。'}
         self.follow = '友達追加ありがとうございます！\nポケロケのサイトから目的のバス停のバス接近情報を表示するURLを入力してもらうと、バス接近情報を通知します。\n詳しい使い方は〜を参照してください。\nhttp://blsetup.city.kyoto.jp/blsp/'
+    
+    def return_bus_text(self, bus_id, keitoList):
+        assert type(bus_id) == int
+        if bus_id == 4:
+            return self.bus[bus_id]
+        else:
+            keito = '\n'.join(keitoList)
+            return f'{self.bus[bus_id]}\n{keito}'
+
 
 class User:
     def __init__(self):
@@ -181,14 +190,32 @@ def handle_message(event):
         text = ''
 
         bus_num = 0
-        text_id = 3
-
-        for i, img in enumerate(imgs):
-            if img.get('src') == "./disp_image_sp/bus_now_app_img_sp.gif" or img.get('src') == './disp_image_sp/bus_img_sp.gif':
-                text_id = min(text_id, i%3)
-                bus_num += 1
+        text_id = 4
+        keitoList = []
         
-        text = txt.bus[text_id]
+        for i, img in enumerate(imgs):
+            # 系統情報
+            if i % 4 == 0:
+                keito = img.get('alt')
+            # バスが来ているかどうかの情報
+            else:
+                # バスが来ている場合
+                if img.get('src') == "./disp_image_sp/bus_now_app_img_sp.gif" or img.get('src') == './disp_image_sp/bus_img_sp.gif':
+                    # 今まで見た中で最も近いものと距離が同じ場合
+                    if text_id == i%4:
+                        keitoList.append(keito)
+                    # 今まで見た中で最も近いものより距離が近い場合
+                    elif text_id > i%4:
+                        text_id = i%4
+                        keitoList = [keito]
+                    bus_num += 1
+
+        # for i, img in enumerate(imgs):
+        #     if img.get('src') == "./disp_image_sp/bus_now_app_img_sp.gif" or img.get('src') == './disp_image_sp/bus_img_sp.gif':
+        #         text_id = min(text_id, i%3)
+        #         bus_num += 1
+        
+        text = txt.return_bus_text(text_id, keitoList)
 
 
         # for i in range(len(imgs)):
