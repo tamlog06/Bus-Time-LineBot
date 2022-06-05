419a88ec881595c8ecab5786e96a9872a01b6352
tam1006 <tamusyun1006@gmail.com>
2022-06-05 18:42:43 +0900

    fix bug: l239

--- main.py
+++ main.py
@@ -236,7 +236,7 @@ def handle_message(event):
         #             text = txt.bus[str(i+1)]
         
         # 前の通知が1駅前のもので、現在のバスの数が前のものより少なければ、バスが到着したと判断して終了
-        if before_id == 1 and (bus_num < bus_num_before or text_id != 1 or keitoList != before_keitoList):
+        if before_id == 1 and (bus_num < bus_num_before or text_id != 1 or len(keitoList) < len(before_keitoList)):
             line_bot_api.push_message(
                 event.source.user_id,
                 TextSendMessage(text=txt.bus['arrive']))
